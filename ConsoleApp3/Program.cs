42858f69218ce41ac75a5e98881579a1643a808a
Laurent Wieser <lwieser@capcod.eu>
2023-10-18 10:33:02 +0200

    init

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string op;
+int a ;
+int b;
+do
+{ 
+    a = InputManager.GetNumberInputLoop("A");
+    op = InputManager.GetOperator();
+    b = InputManager.GetNumberInputLoop("B");
+    int result = a + b;
+    Console.WriteLine("Le résultat est " + result);
+
+} while (op != "bye");
